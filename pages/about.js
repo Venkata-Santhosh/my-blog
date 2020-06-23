8e0cdebe65582d0633729b45d350de6308434bbc
Venkata-Santhosh <santhosh.piduri@gmail.com>
2020-06-23 08:29:24 -0400

    change

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 export default function About() {
   return (
     <div>
-      <div>About us</div>
+      <div>About us!</div>
       <div>
         Back to{' '}
         <Link href="/" as={process.env.BACKEND_URL + '/'}>
