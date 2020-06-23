f970e19da7bdb2afbd7af221d225dcaac5d565df
Venkata-Santhosh <santhosh.piduri@gmail.com>
2020-06-23 12:17:31 -0400

    blog posts

--- pages/index.js
+++ pages/index.js
@@ -29,14 +29,7 @@ const Home = ({posts}) => {
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+        
       </footer>
 
       <style jsx>{`
